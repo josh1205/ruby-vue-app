591b95e2532e5c8067c0588cb282a86f38879721
Joshua Lavieri <joshualavieri@Joshuas-MacBook-Pro.local>
2024-09-10 19:40:02 -0400

    Added subdirectory after removing nested .git

---
+++ backend/config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
+
+  root to: "home#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
